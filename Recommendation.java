18376368acd75a297289675fc4b0daf6bf47585b
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-08-03 22:52:22 -0700

    Update Recommendation.java
    Working solution.

--- Recommendation.java
+++ Recommendation.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,7 +12,7 @@ public class Recommendation {
 		int[] result = new int[2];
 		
 		// adding all the elements in the desired format into the hashmap
-		HashMap<String,ArrayList<String>> hm = new HashMap<String,ArrayList<String>>();
+		 HashMap<String,ArrayList<String>> hm = new HashMap<String,ArrayList<String>>();
 		Set<String> a2= new HashSet<String>();
 		Set<String> s2=new HashSet<String>();
 		for(int i=0;i<str1.length;i++)
@@ -35,19 +34,72 @@ public class Recommendation {
 		// get only the strong connections from the hashmap and delete the entries 
 		
 		HashSet<String> strongConnections = new HashSet<String>();
+		ArrayList<String> toRemove = new ArrayList<String>();
 		for(Map.Entry m : hm.entrySet()) {
 			//get all strong connections as a set
-			ArrayList<String> temp = new ArrayList<String>;
-			temp = m.getValue();
+			ArrayList<String> temp = new ArrayList<String>();
+			temp = (ArrayList<String>) m.getValue();
+			if(temp.contains(str)) {
+				 strongConnections.addAll(temp);
+				 toRemove.add((String) m.getKey());
+			}
+			
 		}
+		//remove all entries that have already been captured as strong connections
+		for (String remove: toRemove) {
+			hm.remove(remove);
+		}
+		//remove the original item from the set to get a count of exact strong connections 
+		strongConnections.remove(str);
+		result[0] = strongConnections.size();
+		result[1]=  getWeakconnections(strongConnections,hm);
+		
+		
 		
 	  return result;	
+	}
+	public static int getWeakconnections(HashSet<String> strongConnections,HashMap<String,ArrayList<String>> hm) {
+		int size=0;
+		HashSet<String> weakConnections = new HashSet<String>();
+		ArrayList<String> toRemove = new ArrayList<String>();
+		//ArrayList<String> toRemove_set = new ArrayList<String>();
+		for(String tempr:strongConnections) {
+		  for(Map.Entry m : hm.entrySet()) {
+			 ArrayList<String> temp = new ArrayList<String>();
+			 temp = (ArrayList<String>) m.getValue();
+			 if(temp.contains(tempr)) {
+				 weakConnections.addAll(temp);
+				 toRemove.add((String)m.getKey());
+			 }
+			
+		  }
+		   
+		}
+		for (String remove: toRemove) {
+			hm.remove(remove);
+		}
+		
+		for (String remove: strongConnections) {
+			weakConnections.remove(remove);
+		}
+		size  = weakConnections.size();
+		//System.out.println(size);
+		if(size>0) {
+			getWeakconnections(weakConnections,hm);
+		}
+		else {
+		return size;
+		}
+		// TODO Auto-generated method stub
+		return size;
+		
 	}
 	public static void main(String args[])
 	{
 		String str="ABC";
-		String[] str1={"first:ABC","first:PQR","second:PQR","second:DFE","second:GHI","third:PQR","third:MNO","fourth:MLK"};
+		String[] str1={"first:ABC","first:PQR","second:PQR","second:DEF","second:GHI","third:PQR","third:MNO","fourth:MLK","fourth:ABC","fifth:DEF","fifth:XYZ","six:MLK","six:ASDF"};
 		int[] a=amz(str,str1);
 		System.out.println(a[0]+" "+a[1]);
 	}
 }
+
