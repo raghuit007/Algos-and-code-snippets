1593586fde6dafb7d09b7cd977f6304336bcd2aa
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-08-09 17:46:33 -0700

    Update Permutations.java
    Added code for permutations of an array. String is similar. Just pass a character array instead. Will add the return function later.

--- Permutations.java
+++ Permutations.java
@@ -1,1 +1,53 @@
 /* Program to print all permutations of a given string- Using recurrsion */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PermutateString {
+	
+
+	public static List<List<Integer>> getPermutations(int pre ,int [] s ) {
+		 List<List<Integer>> result  = new LinkedList<List<Integer>>();
+	     List<Integer> resultSet = new LinkedList<Integer>();
+		
+		int len = s.length;
+		if(pre==len) {  
+			
+			for(int j=0;j<s.length;j++) {
+				resultSet.add(s[j]);
+			}
+			result.add(resultSet);
+			//System.out.println(result.get(0));
+			
+		
+		} 
+		for (int i = pre; i < len; i++) {
+	        int[] permutation = s.clone();
+	         permutation[pre] = s[i];
+	        permutation[i] = s[pre]; 
+	        getPermutations( pre + 1,permutation); 
+		}
+		return result ;
+	    	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] a = {1,2,3};
+		//int prefix = a[0];
+		List<List<Integer>> result  = new LinkedList<List<Integer>>();
+		//Integer [] numTemp = (Integer[])a.clone(); 
+		result= getPermutations(0, a);
+		
+		System.out.println(result.size());
+		
+		
+		
+
+	}
+
+}
+
