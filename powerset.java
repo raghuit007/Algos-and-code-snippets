44d1ff509cc0064edd8c6a9ec85bb66930850285
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-08-10 00:02:50 -0700

    Create powerset.java
    nput: 
    S = {a, b, c, d}
    Output:
    {}, {a} , {b}, {c}, {d}, {a,b}, {a,c},
    {a,d}, {b,c}, {b,d}, {c,d}, {a,b,c}, 
    {a,b,d}, {a,c,d}, {b,c,d}, {a,b,c,d}

---
+++ powerset.java
@@ -0,0 +1,28 @@
+/* program to display powersets of a given set */
+
+
+public class subset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] a = {1,2,3};
+		
+		int setSize = a.length;
+		double powerSize = Math.pow(2, setSize);
+		for(int i=0;i<powerSize;i++) {
+			int temp [] = new int[setSize];
+			System.out.print("{ ");
+			for(int j=0;j<setSize;j++) {
+				//convert to binary 
+				
+				
+				}
+			}
+			System.out.println("}");
+		}
+		
+
+	}
+
+
