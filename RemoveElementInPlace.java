e3d0bbb3fc22c39ee97cfbaffe5591bb7265f6d7
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-08-11 17:45:33 -0700

    Create RemoveElementInPlace.java
    Initial commit. Has array out of bounds exception. Will debug later.

---
+++ RemoveElementInPlace.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int holder=0;
+        for(int i=0;i<nums.length;i++) {
+           if(nums[i]==val) {
+               int temp=i;
+               while(nums[temp]==val) {
+                   //System.out.println(holder++);
+                   temp++;
+                   
+               }
+               int swap = nums[i];
+               nums[i]=nums[temp];
+               nums[temp]=swap;
+               holder++;
+           }
+           else {
+               continue;
+           }
+           System.out.println(nums[i]+" "+i);
+        }
+        return holder;
+    }
+}
