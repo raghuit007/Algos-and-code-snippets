2b6bbf439906d4cef0a84a17dd079a776cff734b
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-08-17 08:36:55 -0700

    Rename LinkedList.java to LinkedListCycle.java
    Logic to be implemented yet.

---
+++ LinkedListCycle.java
@@ -0,0 +1,24 @@
+//find if a linkedlist has a loop 
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        boolean result=false;
+        ListNode temp = head;
+        if (head == null) {
+            return false;
+        }
+        
+        return result;
+    }
+}
