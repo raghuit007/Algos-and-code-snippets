9808d80675ab1f3d7bb0c43cf75ed1a4760c5f11
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-08-18 00:20:18 -0700

    Update LinkedListCycle.java
    working code - no extra space used.

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -20,5 +20,45 @@ public class Solution {
         }
         
         return result;
+    }/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+       boolean result=false;
+        ListNode fast = head;
+        ListNode slow = head;
+        if (head == null) {
+            return false;
+        }
+        if (head.next==head) {
+            return true;
+        }
+            
+        while(slow.next!=null) {
+            if(fast.next.next!=null) 
+            {
+                fast = fast.next.next;
+            }
+            slow = slow.next;
+            if(fast.next==null || fast.next.next==null) {
+                break;
+            }
+            if(slow==fast) {
+                result = true;
+                break;
+            }
+                
+        }
+        return result;
     }
+}
 }
