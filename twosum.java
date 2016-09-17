6db32aa4952846624298126026d574a2e1004170
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-09-17 16:52:55 -0700

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -64,3 +64,24 @@ public class Solution {
         
     }
 }
+
+// intelligent solution with just 1 loop
+
+
+public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        if(nums.length<2) return result;
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            if(!map.containsKey(target-nums[i])){ // if key not present
+                map.put(nums[i], i);
+            }
+            else{
+                result[0]=map.get(target-nums[i]);
+                result[1]=i;
+                break;
+            }
+        }
+        return result;
+    }
