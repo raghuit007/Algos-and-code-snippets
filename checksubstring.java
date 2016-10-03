83cf677305261f0e9301ac8e356ab18828fc2efa
Raghavendra Kumar B <raghavendra.vn512@gmail.com>
2016-10-03 16:00:44 -0700

    Update checksubstring.java

--- checksubstring.java
+++ checksubstring.java
@@ -18,3 +18,41 @@ public class Solution {
 }
 }
 
+/* Another approach (takes more time)
+ public int strStr(String haystack, String needle) {
+        int j=0;
+        int index=0;
+        int temp=0;
+        if (needle.length()==0 ) {
+            return 0;
+        }
+        if (haystack.length()==0) {
+            return -1;
+        }
+        for (int i=0;i<haystack.length();i++) {
+            if (haystack.charAt(i) != needle.charAt(j) )
+            {
+                continue;
+            }
+            else {
+                int count=0;
+                temp=i;
+                while (j<=needle.length()-1 && temp<=haystack.length()-1 && haystack.charAt(temp) == needle.charAt(j)) {
+                    count++;
+                    index =i;
+                    j++;
+                    temp++;
+                    if (j==needle.length() && count==needle.length()) {
+                        return index;
+                        //break;
+                    }
+                }
+                j=0;
+            }
+        }
+        
+        return -1;
+    }
+
+*/
+
